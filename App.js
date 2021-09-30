c0ea72a6a497034d4877042d79fa47ee48c5898e
Neel Ismail <neelismail01@gmail.com>
2021-09-30 11:44:51 -0400

    creating redux slices and stores

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./Redux/store";
+
 // Navigators
 import Main from './Navigators/Main';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Main />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Main />
+      </NavigationContainer>
+    </Provider>
   );
 }
---
+++ Redux/store.js
@@ -0,0 +1,15 @@
+import { createStore, combineReducers } from 'redux';
+
+import { cartItemsReducer } from './cartSlice';
+import { userReducer } from './userSlice';
+import { orderDetailsReducer } from './orderDetailsSlice';
+
+const reducers = combineReducers({
+    cartItems: cartItemsReducer,
+    userDetails: userReducer,
+    orderDetails: orderDetailsReducer,
+})
+
+const store = createStore(reducers);
+
+export default store;
