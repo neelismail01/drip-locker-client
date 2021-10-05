89dfb4281e0ab170276e86c08fccaf870562a060
Neel Ismail <neelismail01@gmail.com>
2021-10-05 10:45:49 -0400

    adding reciepts screen to orders navigator

--- Navigators/OrdersNavigator.js
+++ Navigators/OrdersNavigator.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import OrdersMain from '../Screens/Orders/OrdersMain'
+import Receipt from '../Screens/Orders/Receipt';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,13 @@ const OrdersNavigator = () => {
             <Stack.Screen
                 name="Orders Main"
                 component={OrdersMain}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <Stack.Screen
+                name="Receipt"
+                component={Receipt}
                 options={{
                     headerShown: false,
                 }}
