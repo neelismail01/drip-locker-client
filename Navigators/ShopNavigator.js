c2eefd25adc37984c74aebca0fd8f9e454ed58ab
Neel Ismail <neelismail01@gmail.com>
2021-10-07 01:03:54 -0400

    adding navigator to product page

--- Navigators/ShopNavigator.js
+++ Navigators/ShopNavigator.js
@@ -3,26 +3,38 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import ShopMain from '../Screens/Shop/Landing/ShopMain';
 import BrandMain from '../Screens/Shop/Brand/BrandMain';
+import ProductMain from '../Screens/Shop/Product/ProductMain';
 
 const Stack = createNativeStackNavigator();
 
 const ShopNavigator = () => {
     return (
         <Stack.Navigator>
-            <Stack.Screen
-                name="Shop Main"
-                component={ShopMain}
-                options={{
-                    headerShown: false,
-                }}
-            />
-            <Stack.Screen
-                name="Brand Main"
-                component={BrandMain}
-                options={{
-                    headerShown: false,
-                }}
-            />
+            <Stack.Group>
+                <Stack.Screen
+                    name="Shop Main"
+                    component={ShopMain}
+                    options={{
+                        headerShown: false,
+                    }}
+                />
+                <Stack.Screen
+                    name="Brand Main"
+                    component={BrandMain}
+                    options={{
+                        headerShown: false,
+                    }}
+                />
+            </Stack.Group>
+            <Stack.Group screenOptions={{ presentation: 'modal' }}>
+                <Stack.Screen
+                    name="Product Main"
+                    component={ProductMain}
+                    options={{
+                        headerShown: false,
+                    }}
+                />
+            </Stack.Group>
         </Stack.Navigator>
     )
 }
