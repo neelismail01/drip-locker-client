52cb25662771002c460ce270baf3f292b0e98a1c
Neel Ismail <neelismail01@gmail.com>
2021-10-12 23:29:58 -0400

    adding orders feed to navigation for profile tab

--- Navigators/ProfileNavigator.js
+++ Navigators/ProfileNavigator.js
@@ -6,6 +6,7 @@ import SettingsMain from '../Screens/Profile/Settings/SettingsMain';
 import PersonalInformation from '../Screens/Profile/EditPersonalInformation/PersonalInformation';
 import Addresses from '../Screens/Profile/EditAddress/Addresses';
 import SearchAddress from '../Screens/Profile/EditAddress/SearchAddress';
+import OrdersFeedMain from '../Screens/Profile/OrdersFeed/OrdersFeedMain';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,6 +17,13 @@ const ProfileNavigator = () => {
                 <Stack.Screen
                     name="Profile Main"
                     component={ProfileMain}
+                    options={{
+                        headerShown: false,
+                    }}
+                />
+                <Stack.Screen
+                    name="Orders Feed Main"
+                    component={OrdersFeedMain}
                     options={{
                         headerShown: false,
                     }}
