f3c3ebd8875d85668dc491f76a32f2fe01c3fad6
Neel Ismail <neelismail01@gmail.com>
2021-10-18 04:03:50 -0400

    modifying api calls + component logic

--- Screens/Friends/AddFriend/AddFriendMain.js
+++ Screens/Friends/AddFriend/AddFriendMain.js
@@ -1,121 +1,120 @@
-import React, { useState, useCallback } from "react";
-import { View, SafeAreaView, StyleSheet, ScrollView, TextInput } from "react-native";
-import { useFocusEffect } from '@react-navigation/native'
-import axios from 'axios';
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+} from "react-native";
+import axios from "axios";
 import { BASE_URL } from "@env";
 
-import SearchResults from './SearchResults';
-import ReceivedRequests from './ReceivedRequests';
-import CurrentFriends from './CurrentFriends';
+import SearchResults from "./SearchResults";
+import ReceivedRequests from "./ReceivedRequests";
+import CurrentFriends from "./CurrentFriends";
 
-import { useSelector } from 'react-redux';
-import { selectUserId } from '../../../Redux/userSlice';
+import { useSelector } from "react-redux";
+import { selectUserId } from "../../../Redux/userSlice";
 
 const AddFriendMain = ({ navigation }) => {
-    const [receivedRequests, setReceivedRequests] = useState([]);
-    const [friends, setFriends] = useState([]);
-    const [newFriends, setNewFriends] = useState([]);
+  const [receivedRequests, setReceivedRequests] = useState([]);
+  const [friends, setFriends] = useState([]);
+  const [newFriends, setNewFriends] = useState([]);
 
-    const userId = useSelector(selectUserId);
+  const userId = useSelector(selectUserId);
 
-    const handleAcceptFriendRequest = async (friendRequest) => {
-        try {
-            const requestData = {
-                requestId: friendRequest._id,
-                requester: friendRequest.requester._id,
-                recipient: friendRequest.recipient._id
-            }
-            const response = await axios.put(`${BASE_URL}friends/acceptFriendRequest`, requestData)
-            setReceivedRequests(receivedRequests.filter(request => request._id !== response.data.id))
-        } catch (err) {
-            console.log("Error accepting friend request")
-            console.log(err)
-        }
+  const handleAcceptFriendRequest = async (friendRequest) => {
+    try {
+      const requestData = {
+        requestId: friendRequest._id,
+        requester: friendRequest.requester._id,
+        recipient: friendRequest.recipient._id,
+      };
+      const response = await axios.put(`${BASE_URL}friends/acceptFriendRequest`, requestData);
+    } catch (err) {
+      console.log("Error accepting friend request");
+      console.log(err);
     }
+  };
 
-    const handleSendFriendRequest = async (recipient) => {
-        try {
-            const requestData = {
-                requester: userId,
-                recipient: recipient
-            }
-            const response = await axios.post(`${BASE_URL}friends/sendFriendRequest`, requestData)
-            setNewFriends(newFriends.filter(newFriend => newFriend._id !== response.data))
-        } catch (err) {
-            console.log(err)
-        }
+  const handleSendFriendRequest = async (recipient) => {
+    try {
+      const requestData = {
+        requester: userId,
+        recipient: recipient,
+      };
+      const response = await axios.post(`${BASE_URL}friends/sendFriendRequest`, requestData);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    const handleGoToFriendProfile = (friendUserId, friendName) => {
-        navigation.navigate('Friend Profile Main', { friendUserId, friendName })
+  const handleGoToFriendProfile = (friendUserId, friendName) => {
+    navigation.navigate("Friend Profile Main", { friendUserId, friendName });
+  };
+
+  const handleSearch = async (text) => {
+    try {
+      if (text.length > 1) {
+        const response = await axios.get(`${BASE_URL}friends/search/${userId}?searchTerm=${text}`);
+        setReceivedRequests(response.data.friendRequests);
+        setFriends(response.data.friends);
+        setNewFriends(response.data.newUsers);
+      } else if (text.length === 0) {
+        setReceivedRequests([]);
+        setFriends([]);
+        setNewFriends([]);
+      }
+    } catch (err) {
+      console.log("Error searching for user");
+      console.log(err);
     }
+  };
 
-    const handleSearch = async (text) => {
-        try {
-            if (text.length > 2) {
-                const response = await axios.get(`${BASE_URL}friends/search/${userId}?searchTerm=${text}`);
-                setReceivedRequests(response.data.friendRequests);
-                setFriends(response.data.friends);
-                setNewFriends(response.data.newUsers);
-            } else if (text.length === 0) {
-                setReceivedRequests([]);
-                setFriends([]);
-                setNewFriends([]);
-            }
-        } catch (err) {
-            console.log("Error searching for user")
-            console.log(err);
-        }
-    }
-
-    return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: "white"}}>
-            <TextInput
-                placeholder="Add friends on OrderBud..."
-                style={styles.enterAddressField}
-                onChangeText={text => handleSearch(text)}
-            />
-            <ScrollView>
-                {
-                    friends.length > 0 &&
-                    <CurrentFriends
-                        friends={friends}
-                        userId={userId}
-                        handleGoToFriendProfile={handleGoToFriendProfile}
-                    />
-                }
-                {
-                    receivedRequests.length > 0 &&
-                    <ReceivedRequests
-                        receivedRequests={receivedRequests}
-                        handleAcceptFriendRequest={handleAcceptFriendRequest}
-                    />
-                }
-                {
-                    newFriends.length > 0 &&
-                    <SearchResults
-                        newFriends={newFriends}
-                        handleSendFriendRequest={handleSendFriendRequest}
-                    />
-                }
-            </ScrollView>
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+      <TextInput
+        placeholder="Add friends on OrderBud..."
+        style={styles.enterAddressField}
+        onChangeText={(text) => handleSearch(text)}
+      />
+      <ScrollView>
+        {friends.length > 0 && (
+          <CurrentFriends
+            friends={friends}
+            userId={userId}
+            handleGoToFriendProfile={handleGoToFriendProfile}
+          />
+        )}
+        {receivedRequests.length > 0 && (
+          <ReceivedRequests
+            receivedRequests={receivedRequests}
+            handleAcceptFriendRequest={handleAcceptFriendRequest}
+          />
+        )}
+        {newFriends.length > 0 && (
+          <SearchResults
+            newFriends={newFriends}
+            handleSendFriendRequest={handleSendFriendRequest}
+          />
+        )}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    enterAddressField: {
-        backgroundColor: "white",
-        padding: 15,
-        fontSize: 16,
-        shadowColor: '#a6a6a6',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.5,
-        fontSize: 14,
-        borderRadius: 15,
-        marginBottom: 30,
-        marginHorizontal: 20,
-    }
-})
+  enterAddressField: {
+    backgroundColor: "white",
+    padding: 15,
+    fontSize: 16,
+    shadowColor: "#a6a6a6",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    fontSize: 14,
+    borderRadius: 15,
+    marginVertical: 20,
+    marginHorizontal: 20,
+  },
+});
 
 export default AddFriendMain;
