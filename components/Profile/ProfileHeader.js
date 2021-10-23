f952151be43c7d9e6865b342ea86426a4078bdd4
Neel Ismail <neelismail01@gmail.com>
2021-10-23 18:58:55 -0400

    hiding settings icon on other user's profiles

---
+++ components/Profile/ProfileHeader.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { Icon } from 'react-native-elements';
+
+const ProfileHeader = ({ handleGoToSettings, showSettingsIcon }) => {
+    return (
+        <View style={styles.headerContainer}>
+            <Text style={styles.header}>Profile</Text>
+            {
+                showSettingsIcon &&
+                <View style={styles.iconContainer}>
+                    <TouchableOpacity
+                        onPress={handleGoToSettings}
+                        style={styles.icon}
+                    >
+                        <Icon name="cog" type="font-awesome-5" color="black" size={20} />
+                    </TouchableOpacity>
+                </View>
+            }
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginHorizontal: 20,
+        marginBottom: 10
+    },
+    header: {
+        fontSize: 24,
+        fontWeight: "bold",
+    },
+    iconContainer: {
+        justifyContent: "flex-end",
+        flexDirection: "row"
+    },
+    icon: {
+        marginLeft: 20
+    }
+});
+
+export default ProfileHeader;
