9df3118195434a898bad234e27536d8592dd8ba9
Neel Ismail <neelismail01@gmail.com>
2021-10-23 18:57:08 -0400

    hiding liked orders on friend's tab

--- components/Profile/OrdersFilter.js
+++ components/Profile/OrdersFilter.js
@@ -2,22 +2,29 @@ import React from "react";
 import { StyleSheet, View, TouchableOpacity } from "react-native";
 import { Icon } from 'react-native-elements';
 
-const OrdersFilter = ({ activeTab, handleChangeToOrdersTab, handleChangeToLikedTab }) => {
+import { useSelector } from 'react-redux';
+import { selectUserId } from '../../Redux/userSlice';
+
+const OrdersFilter = ({ activeTab, profileId, handleChangeToOrdersTab, handleChangeToLikedTab }) => {
+    const userId = useSelector(selectUserId);
 
     return (
         <View style={styles.tabsRow}>
             <TouchableOpacity
-                style={[styles.tabContainer, activeTab === 0 && styles.activeTab]}
+                style={profileId !== userId ? styles.orderTabOnlyContainer : [styles.tabContainer, activeTab === 0 && styles.activeTab]}
                 onPress={handleChangeToOrdersTab}
             >
                 <Icon name="receipt" type="font-awesome-5" color="black" size={18} />
             </TouchableOpacity>
-            <TouchableOpacity
-                style={[styles.tabContainer, activeTab === 1 && styles.activeTab]}
-                onPress={handleChangeToLikedTab}
-            >
-                <Icon name="favorite" type="material" color="black" size={20} />
-            </TouchableOpacity>
+            {
+                profileId === userId &&
+                <TouchableOpacity
+                    style={[styles.tabContainer, activeTab === 1 && styles.activeTab]}
+                    onPress={handleChangeToLikedTab}
+                >
+                    <Icon name="favorite" type="material" color="black" size={20} />
+                </TouchableOpacity>
+            }
         </View>
     )
 };
@@ -26,6 +33,13 @@ const styles = StyleSheet.create({
     tabsRow: {
         width: "100%",
         flexDirection: "row"
+    },
+    orderTabOnlyContainer: {
+        width: "50%",
+        paddingVertical: 10,
+        alignItems: "center",
+        borderBottomWidth: 1,
+        borderBottomColor: "grey"
     },
     tabContainer: { 
         width: "50%",
