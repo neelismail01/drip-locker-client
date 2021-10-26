b2444ee0ae39afc0bc87a57108129a2e7da3fdef
Neel Ismail <neelismail01@gmail.com>
2021-10-25 21:11:17 -0400

    delete aws cognito pool - no longer needed

--- util/UserPool.js
+++
@@ -1,9 +0,0 @@
-import { CognitoUserPool } from 'amazon-cognito-identity-js';
-import { AWS_COGNITO_POOL_ID, AWS_APP_CLIENT_ID } from "@env";
-
-const poolData = {
-    UserPoolId: AWS_COGNITO_POOL_ID,
-    ClientId: AWS_APP_CLIENT_ID
-}
-
-export default new CognitoUserPool(poolData);
