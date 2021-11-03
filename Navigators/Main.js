4c1cfad7e61027391304987d624a3705ed850d93
Neel Ismail <neelismail01@gmail.com>
2021-11-03 15:50:28 -0400

    checking for access token to determine login state

--- Navigators/Main.js
+++ Navigators/Main.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Icon } from 'react-native-elements'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import AuthenticationNavigator from './AuthenticationNavigator';
 import ShopNavigator from './ShopNavigator';
 import FriendsNavigator from './FriendsNavigator';
 import ProfileNavigator from './ProfileNavigator';
 
-import { useSelector } from 'react-redux';
-import { selectIsLoggedIn } from '../Redux/userSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectIsLoggedIn, setAccessToken } from '../Redux/userSlice';
 
 const Tab = createBottomTabNavigator();
 
 const Main = () => {
 
     const isLoggedIn = useSelector(selectIsLoggedIn);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const checkForUserToken = async () => {
+            try {
+                const accessToken = await AsyncStorage.getItem('access_token');
+                if (accessToken !== null) {
+                    dispatch(setAccessToken(accessToken));
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        checkForUserToken();
+    }, [])
 
     if (isLoggedIn) {
         return (
