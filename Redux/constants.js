68a69af98a3ce601d88a8f94ea63c123f504ad69
Neel Ismail <neelismail01@gmail.com>
2021-11-06 20:23:29 -0400

    removing constants for unused redux actions

--- Redux/constants.js
+++ Redux/constants.js
@@ -1,14 +1,3 @@
-export const ADD_TO_CART = "ADD_TO_CART";
-export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
-export const UPDATE_ITEM_QUANTITY = "UPDATE_ITEM_QUANTITY";
-export const CLEAR_CART = "CLEAR_CART";
-
+export const SET_ACCESS_TOKEN = "SET_ACCESS_TOKEN";
 export const SET_USER_INFO = "SET_USER_INFO";
 export const CLEAR_USER = "CLEAR_USER";
-
-export const SET_ACCESS_TOKEN = "SET_ACCESS_TOKEN";
-export const SET_NAME = "SET_NAME";
-export const SET_EMAIL = "SET_EMAIL";
-export const SET_ADDRESS = "SET_ADDRESS";
-export const SET_PASSWORD = "SET_PASSWORD";
-export const CLEAR_REGISTER = "CLEAR_REGISTER";
