fe1249d11b215329b14e8c9de65d1834d816dbcd
Neel Ismail <neelismail01@gmail.com>
2021-11-11 15:29:22 -0500

    removing user search from friend tab

--- Navigators/FriendsNavigator.js
+++ Navigators/FriendsNavigator.js
@@ -4,45 +4,33 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import FeedMain from '../Screens/Friends/Feed/FeedMain';
 import ProfileMain from '../Screens/Friends/Profile/UserProfile';
 import OrdersFeedMain from '../Screens/Friends/OrdersFeed/OrdersFeedMain';
-import AddFriendMain from '../Screens/Friends/AddFriend/AddFriendMain';
 
 const Stack = createNativeStackNavigator();
 
 const FriendsNavigator = () => {
     return (
         <Stack.Navigator>
-            <Stack.Group>
-                <Stack.Screen
-                    name="Feed Main"
-                    component={FeedMain}
-                    options={{
-                        headerShown: false,
-                    }}
-                />
-                <Stack.Screen
-                    name="Profile Main"
-                    component={ProfileMain}
-                    options={{
-                        headerShown: false,
-                    }}
-                />
-                <Stack.Screen
-                    name="Orders Feed Main"
-                    component={OrdersFeedMain}
-                    options={{
-                        headerShown: false,
-                    }}
-                />
-            </Stack.Group>
-            <Stack.Group screenOptions={{ presentation: 'modal' }}>
-                <Stack.Screen
-                    name="Add Friend Main"
-                    component={AddFriendMain}
-                    options={{
-                        headerShown: false,
-                    }}
-                />
-            </Stack.Group>
+            <Stack.Screen
+                name="Feed Main"
+                component={FeedMain}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <Stack.Screen
+                name="Profile Main"
+                component={ProfileMain}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <Stack.Screen
+                name="Orders Feed Main"
+                component={OrdersFeedMain}
+                options={{
+                    headerShown: false,
+                }}
+            />
         </Stack.Navigator>
     )
 }
