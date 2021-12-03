a91a2eabfdf562e577a10b2ed9efe09a98bd70c8
Neel Ismail <neelismail01@gmail.com>
2021-12-03 00:06:51 -0500

    deleting unused imported components

--- Screens/Trending/Landing/TrendingMain.js
+++ Screens/Trending/Landing/TrendingMain.js
@@ -1,16 +1,7 @@
 import React, { useState, useCallback } from "react";
 import {
-  StyleSheet,
-  SafeAreaView,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  ScrollView,
-  ActivityIndicator,
-  RefreshControl
+  SafeAreaView
 } from "react-native";
-import { Icon } from "react-native-elements";
 import { useFocusEffect } from "@react-navigation/native";
 import axios from "axios";
 import { AWS_BASE_URL } from "@env";
@@ -22,7 +13,7 @@ import TrendingBrands from "./TrendingBrands";
 
 const TrendingMain = () => {
   const [trendingBrands, setTrendingBrands] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
   const accessToken = useSelector(selectAccessToken);
 
@@ -39,10 +30,9 @@ const TrendingMain = () => {
 
   useFocusEffect(
     useCallback(() => {
-      setLoading(true);
       axios
         .get(`${AWS_BASE_URL}orders/trending/brands`, config)
-        .then((response) => {
+        .then(response => {
           setTrendingBrands(response.data.body);
         })
         .catch((err) => {
@@ -67,45 +57,5 @@ const TrendingMain = () => {
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  trendingBrandRow: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    padding: 20,
-  },
-  brandDetails: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  brandLogo: {
-    height: 50,
-    width: 50,
-    resizeMode: "contain",
-    marginHorizontal: 20,
-  },
-  brandName: {
-    fontWeight: "bold",
-    fontSize: 14,
-  },
-  brandNameAndWebsite: {
-    height: "auto",
-  },
-  brandStats: {
-    color: "grey",
-    fontSize: 12,
-  },
-  shopButton: {
-    backgroundColor: "#efefef",
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 10,
-  },
-  shopText: {
-    fontSize: 12,
-    fontWeight: "bold",
-  },
-});
 
 export default TrendingMain;
