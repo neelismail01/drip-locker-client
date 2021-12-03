cce5519cdd8ae7c6d0e99dfd23eb3791755748cd
Neel Ismail <neelismail01@gmail.com>
2021-12-03 01:59:26 -0500

    removing unnecessary style property

--- Screens/Friends/Feed/EmptyFeed.js
+++ Screens/Friends/Feed/EmptyFeed.js
@@ -17,7 +17,6 @@ const EmptyFeed = ({ loading }) => {
 
 const styles = StyleSheet.create({
     container: {
-        flexGrow: 1,
         justifyContent: "center",
         alignItems: "center",
     },
