cf500df6586cad02afe3039c3ae82cda5e6a450e
Neel Ismail <neelismail01@gmail.com>
2021-12-17 17:33:51 -0500

    removing unused touchable opacity import

--- Screens/Trending/Landing/TrendingBrandRow.js
+++ Screens/Trending/Landing/TrendingBrandRow.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, Image, TouchableOpacity, StyleSheet } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
 import { Icon } from "react-native-elements";
 
 const TrendingBrandRow = ({ rank, brand }) => {
